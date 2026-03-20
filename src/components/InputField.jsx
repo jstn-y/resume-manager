41846c0ffae3dec0836838bc1804a22826cd5f9f
Justin <jstnyao25@gmail.com>
2026-03-19 22:43:23 -0400

    Updated formatting

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,13 +1,14 @@
 export default function InputField({ label, ...props }) {
-  return (
-    <div className="flex flex-col gap-1">
-      <label className="text-[#aaa] text-xs uppercase tracking-widest">
-        {label}
-      </label>
-      <input
-        className="bg-[#111] border border-[#2e2e2e] rounded-lg px-4 py-3 text-[#f0ece3] w-full"
-        {...props}
-      />
-    </div>
-  )
+    return (
+        <div className="flex flex-col gap-1">
+            <label className="text-[#aaa] text-xs uppercase tracking-widest">
+                {label}
+            </label>
+        <input
+            className="bg-[#111] border border-[#2e2e2e] rounded-lg px-4 py-3 text-[#f0ece3] w-full"
+            {...props}
+        />
+        </div>
+    )
 }
+
