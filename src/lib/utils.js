24eea802c994ed46ceb662ebd8624a2710d9bc0a
Justin <jstnyao25@gmail.com>
2026-03-23 00:19:54 -0400

    Added shadcn dependencies

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,14 +1,6 @@
-import { clsx } from "clsx"
+import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs) {
-  return twMerge(clsx(inputs))
-}
-
-export function formatDate(date) {
-  return new Date(date).toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric'
-  })
+  return twMerge(clsx(inputs));
 }
